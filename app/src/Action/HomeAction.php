5501fccd3ee06e0ab8703a758d6b5a927db46a87
falcon <falcon_chen@qq.com>
2019-07-08 20:03:06 +0800

    update app validate

--- app/src/Action/HomeAction.php
+++ app/src/Action/HomeAction.php
@@ -1,17 +1,16 @@
 <?php
 namespace App\Action;
 
-use Slim\Views\Twig;
-use Psr\Log\LoggerInterface;
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Psr\Http\Message\ResponseInterface as Response;
-use Carlosocarvalho\SimpleInput\Input\Input;
+use App\Helper\Hash;
+use App\Helper\JsonRenderer;
+use App\Helper\JsonRequest;
 use App\Model\User;
 use App\Validation\Validator;
-use App\Helper\Hash;
-use App\Helper\Acl;
-use App\Helper\JsonRequest;
-use App\Helper\JsonRenderer;
+use Carlosocarvalho\SimpleInput\Input\Input;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Log\LoggerInterface;
+use Slim\Views\Twig;
 
 final class HomeAction
 {
@@ -22,49 +21,44 @@ final class HomeAction
     private $session;
     private $jsonRequest;
 
-    public function __construct(JsonRequest $jsonRequest,Twig $view, LoggerInterface $logger, $hash,$auth)
+    public function __construct(JsonRequest $jsonRequest, Twig $view, LoggerInterface $logger, $hash, $auth)
     {
-        $this->view     = $view;
-        $this->logger   = $logger;
-        $this->hash     = $hash;
-        $this->auth     = $auth;
-        $this->session  = new \App\Helper\Session;
-        $this->jsonRequest  = new JsonRequest();
-        $this->JsonRender   = new JsonRenderer();
-       
+        $this->view = $view;
+        $this->logger = $logger;
+        $this->hash = $hash;
+        $this->auth = $auth;
+        $this->session = new \App\Helper\Session;
+        $this->jsonRequest = new JsonRequest();
+        $this->JsonRender = new JsonRenderer();
+
     }
 
     public function dispatch(Request $request, Response $response, $args)
     {
         $this->logger->info("Home page action dispatched");
-        
-        $this->view->render($response, 'home.twig',[
+        $this->view->render($response, 'home.twig', [
             'user' => User::all(),
-            
-            ]);
+
+        ]);
         return $response;
     }
 
     public function dashboard(Request $request, Response $response, $args)
     {
         $flash = $this->session->get('flash');
-        return $this->view->render($response, 'dashboard.twig',['flash' => $flash ] );
+        return $this->view->render($response, 'dashboard.twig', ['flash' => $flash]);
     }
 
     public function logout(Request $request, Response $response, $args)
     {
-        $session    = new \App\Helper\Session;
+        $session = new \App\Helper\Session;
         $session::destroy();
         return $response->withRedirect('login');
     }
 
-    public function login(Request $request, Response $response, $args){
-        $this->view->render($response, 'login.twig',
-            ['csrf' => [
-                        'name' => $request->getAttribute('csrf_name'),
-                        'value' => $request->getAttribute('csrf_value'),
-                      ],
-            ]);
+    public function login(Request $request, Response $response, $args)
+    {
+        $this->view->render($response, 'login.twig');
         return $response;
     }
 
@@ -74,51 +68,46 @@ final class HomeAction
 
         $user_id = $jsonRequest->getRequestParam('password');
 
-           $data = [
-                'user_id' => $user_id
-            ];
+        $data = [
+            'user_id' => $user_id,
+        ];
 
-            $response = $this->JsonRender->render($response, 200, $data);
-            return $response;
+        $response = $this->JsonRender->render($response, 200, $data);
+        return $response;
     }
 
     public function loginPost(Request $request, Response $response, $args)
     {
         $identifier = Input::post('identifier');
-        $password   = Input::post('password');
+        $password = Input::post('password');
         $v = new Validator(new User);
         $v->validate([
-        'identifier'    => [$identifier, 'required|email'],
-        'password'      => [$password, 'required']
-        ]);;
+            'identifier' => [$identifier, 'required|email'],
+            'password' => [$password, 'required'],
+        ]);
         if ($request->getAttribute('csrf_status') === false) {
             $flash = 'CSRF faiure';
-            $this->view->render($response, 'login.twig',['errors' => $v->errors(),'flash' => $flash,'request' => $request]);
-        }else{
-            if($v->passes()){            
+            $this->view->render($response, 'login.twig', ['errors' => $v->errors(), 'flash' => $flash, 'request' => $request]);
+        } else {
+            if ($v->passes()) {
                 $user = User::where('username', $identifier)->orWhere('email', $identifier)->first();
-                if($user && $this->hash->passwordCheck($password, $user->password)){                
-                    $this->session->set($this->auth['session'],$user->id);
-                    $this->session->set($this->auth['group'],$user->group_id);
+                if ($user && $this->hash->passwordCheck($password, $user->password)) {
+                    $this->session->set($this->auth['session'], $user->id);
+                    $this->session->set($this->auth['group'], $user->group_id);
                     return $response->withRedirect('dashboard');
-                }
-                else{
-                    $flash = 'Sorry, you couldn\'t be logged in.';            
-                    $this->view->render($response, 'login.twig',['errors' => $v->errors(),'flash' => $flash,'request' => $request]);
+                } else {
+                    $flash = 'Sorry, you couldn\'t be logged in.';
+                    $this->view->render($response, 'login.twig', ['errors' => $v->errors(), 'flash' => $flash, 'request' => $request]);
                 }
 
-            }else{        
+            } else {
                 $this->view->render($response, 'login.twig',
                     ['errors' => $v->errors(),
-                    'request' => $request,
-                    'csrf' => [
-                        'name' => $request->getAttribute('csrf_name'),
-                        'value' => $request->getAttribute('csrf_value'),
-                      ],
+                        'request' => $request,
                     ]);
             }
         }
-        
+
         return $response;
     }
 
@@ -130,32 +119,33 @@ final class HomeAction
 
     public function registerPost(Request $request, Response $response, $args)
     {
-        $email      = Input::post('email');
-        $username   = Input::post('username');
-        $password   = Input::post('password');
+
+        $email = Input::post('email');
+        $username = Input::post('username');
+        $password = Input::post('password');
 
         $passwordConfirm = Input::post('password_confirm');
         $v = new Validator(new User);
         $v->validate([
-            'email'     => [$email, 'required|email|uniqueEmail'],
-            'username'  => [$username, 'required|alnumDash|max(20)|uniqueUsername'],
-            'password'  => [$password, 'required|min(6)'],
-            'password_confirm' => [$passwordConfirm, 'required|matches(password)']
+            'email' => [$email, 'required|email|uniqueEmail'],
+            'username' => [$username, 'required|alnumDash|max(20)|uniqueUsername'],
+            'password' => [$password, 'required|min(6)'],
+            'password_confirm' => [$passwordConfirm, 'required|matches(password)'],
         ]);
 
         if ($v->passes()) {
             $user = new User();
-            $user->email    = $email;
+            $user->email = $email;
             $user->username = $username;
             $user->password = $this->hash->password($password);
             $user->group_id = 3;
             $user->save();
             $flash = "You have been registered.";
-        }else{
+        } else {
             $flash = "registration failed.";
         }
-        
-        $this->view->render($response, 'register.twig',['errors' => $v->errors(),'flash' => $flash,'request' => $request]);
+
+        $this->view->render($response, 'register.twig', ['errors' => $v->errors(), 'flash' => $flash, 'request' => $request->getParsedBody()]);
         return $response;
     }
 }
--- app/src/Validation/Validator.php
+++ app/src/Validation/Validator.php
@@ -2,22 +2,22 @@
 
 namespace App\Validation;
 
-use Violin\Violin;
-
 use App\Model\User;
+use Violin\Violin;
 
 class Validator extends Violin
 {
-    function __construct(User $user) {
+    public function __construct(User $user)
+    {
         $this->user = $user;
 
         $this->addFieldMessages([
             'email' => [
-                'uniqueEmail' => 'email already in use'
+                'uniqueEmail' => 'email already in use',
             ],
             'username' => [
-                'uniqueUsername' => 'username already in use'
-            ]
+                'uniqueUsername' => 'username already in use',
+            ],
         ]);
     }
 
@@ -25,11 +25,35 @@ class Validator extends Violin
     {
         $user = $this->user->where('email', $value);
 
-        return ! (bool) $user->count();
+        return !(bool) $user->count();
     }
 
     public function validate_uniqueUsername($value, $input, $args)
     {
-        return ! (bool) $this->user->where('username', $value)->count();
+        return !(bool) $this->user->where('username', $value)->count();
+    }
+
+    /**
+     * Method to help skip a rule if a value is empty, since we
+     * don't need to validate an empty value. If the rule to
+     * call specifically doesn't allowing skipping, then
+     * we don't want skip the rule.
+     * fix the 0 value skipRule
+     *
+     * @param  array $ruleToCall
+     * @param  mixed $value
+     *
+     * @return null
+     */
+    protected function canSkipRule($ruleToCall, $value)
+    {
+        return (
+            (is_array($ruleToCall) &&
+                method_exists($ruleToCall[0], 'canSkip') &&
+                $ruleToCall[0]->canSkip()) &&
+            strlen($value) == 0 && empty($value) &&
+            !is_array($value)
+        );
+
     }
 }
