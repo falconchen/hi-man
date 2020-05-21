6f5263aa4aff7a513c21db6b81a5e424c1a74599
falcon <falcon_chen@qq.com>
2020-05-21 19:31:56 +0800

    add back to top button

--- public/js/site.js
+++ public/js/site.js
@@ -14,4 +14,23 @@ $(document).ready(function() {
         }
     });
 
+
+    /*Scroll to top when arrow up clicked BEGIN*/
+    // $(window).scroll(function() {
+    //     var height = $(window).scrollTop();
+    //     if (height > 100) {
+    //         $('#back2Top').fadeIn();
+    //     } else {
+    //         $('#back2Top').fadeOut();
+    //     }
+    // });
+
+    
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+
 });
