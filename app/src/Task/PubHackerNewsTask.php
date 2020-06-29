b0ce1731bc544b1e8f82c36ed3f046583e81fdc9
falcon <falcon_chen@qq.com>
2020-06-29 23:27:36 +0800

    add a space in title

--- app/src/Task/PubHackerNewsTask.php
+++ app/src/Task/PubHackerNewsTask.php
@@ -96,7 +96,7 @@ class PubHackerNewsTask extends BaseTaskAbstract
             $isCreate = true;
             $post = Post::firstOrNew(['post_name' => $postName]); //新建,如果是trash或draft直接覆盖
             $post->post_author = 12;
-            $post->post_title = 'Hacker News简讯 ' . date('Y-m-d', $localTimeStamp);
+            $post->post_title = 'Hacker News 简讯 ' . date('Y-m-d', $localTimeStamp);
             $post->post_date = $post->post_modified;
             $post->post_date_local = date('Y-m-d H:i:s', $localTimeStamp);
             $post->post_status = 'publish';
