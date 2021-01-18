eef4ba7fe70545232d4961f48c62ad4860ec60cf
falcon <falcon_chen@qq.com>
2021-01-18 18:20:58 +0800

    update jwt

--- app/src/Api/Tokens.php
+++ app/src/Api/Tokens.php
@@ -18,13 +18,54 @@ final class Tokens extends \App\Helper\ApiAction
 
         
 
-        // $uuid = Uuid::uuid4();
+        //$uuid = Uuid::uuid4();
+        $requested_scopes = $request->getParsedBody() ?: [];
 
-        // printf(
-        //     "UUID: %s\nVersion: %d\n",
-        //     $uuid->toString(),
-        //     $uuid->getFields()->getVersion()
-        // );
+        $valid_scopes = [
+            "collections.create",
+            "collections.read",
+            "collections.update",
+            "collections.delete",
+            "collections.list",
+            "collections.all"
+        ];
+    
+        $scopes = array_filter($requested_scopes, function ($needle) use ($valid_scopes) {
+            return in_array($needle, $valid_scopes);
+        });
+    
+        $now = new \DateTime();
+        $future = new \DateTime("now +2 hours");
+        $server = $request->getServerParams();
+    
+        
+        //$jti = (new Base62)->encode(random_bytes(16));
+        $jti = Uuid::uuid4()->toString();
+    
+        $payload = [
+            "iat" => $now->getTimeStamp(),
+            "exp" => $future->getTimeStamp(),
+            "jti" => $jti,
+            "sub" => $server["PHP_AUTH_USER"],
+            "scope" => $scopes
+        ];
+    
+        $secret = getenv("JWT_SECRET");
+        $token = JWT::encode($payload, $secret, "HS256");
+    
+        $data["token"] = $token;
+        $data["expires"] = $future->getTimeStamp();
+    
+        return $response->withStatus(201)
+            ->withHeader("Content-Type", "application/json")
+            ->write(json_encode($data, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+
+
+        printf(
+            "UUID: %s\nVersion: %d\n",
+            $uuid->toString(),
+            $uuid->getFields()->getVersion()
+        );
         
     }
 }
