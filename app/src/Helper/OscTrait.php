ad21320f76f110aaad781785ac8fe0904e55c808
falcon <falcon_chen@qq.com>
2021-04-10 01:31:55 +0800

    update warning text

--- app/src/Helper/OscTrait.php
+++ app/src/Helper/OscTrait.php
@@ -73,7 +73,7 @@ trait OscTrait {
         if (isset($oscSyncOptions['privacy']) && $oscSyncOptions['privacy'] == 0) {
 
             $postLink = rtrim(hiGetSettings('app')['url'],'/'). $this->c->router->pathFor('post',['name'=>$post->post_name]);
-            $postArr['content'] = sprintf('<blockquote style="margin-bottom:8px;background-color: cornsilk;border-left: 8px solid burlywood;">以下内容已加密，请自行解密或使用<a href="%s">🔧 解密链接</a> 查看</blockquote>',$postLink);
+            $postArr['content'] = sprintf('<blockquote style="margin-bottom:8px;background-color: cornsilk;border-left: 8px solid burlywood;">以下内容已加密，请自行解密或使用<a href="%s">🔧解密链接</a> 查看</blockquote>',$postLink);
 
             $postArr['content'] .= base64_encode($post->post_content); // 用base64编码公开的内容
             
